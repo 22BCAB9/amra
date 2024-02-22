92c78288fb9ff6859e12f8a1037150f8c72a7b2b
22bcab16 <155622694+22bcab16@users.noreply.github.com>
2024-02-22 14:43:49 +0530

    Update multiplexer.py

--- multiplexer.py
+++ multiplexer.py
@@ -31,7 +31,7 @@ def mux(input0: int, input1: int, select: int) -> int:
         ...
     ValueError: Inputs and select signal must be 0 or 1
     """
-    if all(i in (0, 1) for i in (input0, input1, select)):
+    if all(i in (0, 1) for i in (input0, input1,input2,select)):
         return input1 if select else input0
     raise ValueError("Inputs and select signal must be 0 or 1")
 
